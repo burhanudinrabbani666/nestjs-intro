1a45f665f54e0e996a64a64be0c4bed58ee2c670
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-15 00:17:16 +0700

    adding route for GET and POST

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  getHello() {
-    return 'Hello From users route';
+  @Get('{/:id{/:optional}}')
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params);
+    console.log(query);
+    return `You Send GET Request to /users endpoint with`;
+  }
+
+  @Post()
+  public createUsers(@Body() body: any) {
+    console.log(body);
+    return 'You Sent a POST to users endpoint';
   }
 }
