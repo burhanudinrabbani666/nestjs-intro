3f3fd8a467df3a9a7e8e77014cab7de9411dc49d
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-15 22:36:08 +0700

    Installing and config typeORM to Local Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app/app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [UsersModule, PostsModule, AuthModule],
+    imports: [
+        UsersModule,
+        PostsModule,
+        AuthModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            database: 'nestjs_blog',
+            username: 'rabbani',
+            password: 'rabbani',
+            synchronize: true,
+            entities: [],
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
