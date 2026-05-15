338e549beaccb606a6f420036572db611ed6e425
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-15 13:51:46 +0700

    create and implement getOneById service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,37 @@
-import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-users.dto';
 import { GetUsersParamsDto } from './dto/get-users-params.dto';
 import { PatchUserDto } from './dto/patch-users.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(
+    // This is injection Depedency
+    private readonly usersService: UsersService,
+  ) {}
+
   @Get('{/:id}')
-  public getUsers(@Param() getUsersParamsDto: GetUsersParamsDto) {
-    return getUsersParamsDto;
+  public getUsers(
+    @Param() getUsersParamsDto: GetUsersParamsDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    if (getUsersParamsDto.id) {
+      return this.usersService.findOneById(getUsersParamsDto.id);
+    }
+
+    return this.usersService.findAll(getUsersParamsDto, limit, page);
   }
 
   @Post()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { GetUsersParamsDto } from './dto/get-users-params.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public findAll(
+    getUserParamsDto: GetUsersParamsDto,
+    limit: number,
+    page: number,
+  ) {
+    console.log(getUserParamsDto);
+    console.log(limit);
+    console.log(page);
+
+    return [
+      {
+        firstName: 'burhanudin',
+        email: 'bani@bani.io',
+      },
+      {
+        firstName: 'Nico',
+        email: 'nico@nico.io',
+      },
+      {
+        firstName: 'Aziz',
+        email: 'aziz@aziz.io',
+      },
+    ];
+  }
+
+  public findOneById(id: number) {
+    return {
+      id,
+      firstName: 'Burhanudin',
+      email: 'bani@bani.io',
+    };
+  }
+}
