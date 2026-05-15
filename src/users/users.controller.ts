b72c7f6821bdbcdfdc000de8538b135db7d8fa03
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-15 07:25:28 +0700

    Validating query params with default value pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,27 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
   Headers,
   Ip,
   Param,
   ParseIntPipe,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('{/:id}')
-  public getUsers(@Param('id', ParseIntPipe) id: number | undefined) {
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
     console.log(id);
+    console.log(limit);
+    console.log(page);
     return `You Send GET Request to /users endpoint with`;
   }
 
