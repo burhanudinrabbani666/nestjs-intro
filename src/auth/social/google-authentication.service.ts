69571d26acf41c8b1d683941d33a3acc893e1270
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-20 22:05:20 +0700

    Initialize Google auth client

--- src/auth/social/google-authentication.service.ts
+++ src/auth/social/google-authentication.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { type ConfigType } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import jwtConfig from '../config/jwt.config';
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+    private oauthClient: OAuth2Client;
+    constructor(
+        /**
+         * Inject jwtConfiguration
+         */
+        @Inject(jwtConfig.KEY)
+        private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    ) {
+        const clientId = this.jwtConfiguration.googleClientId;
+        const clientSecret = this.jwtConfiguration.googleClientSecret;
+        this.oauthClient = new OAuth2Client(clientId, clientSecret);
+    }
+
+    onModuleInit() {
+        const clientId = this.jwtConfiguration.googleClientId;
+        const clientSecret = this.jwtConfiguration.googleClientSecret;
+        this.oauthClient = new OAuth2Client(clientId, clientSecret);
+    }
+}
